8e0521ff1a204fe9088c41e21a0bc8829b979216
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-06-03 11:35:13 +0530

    Create login page

---
+++ src/components/login/login.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import loginImg from './img/loginIMG.png';
+
+export class login extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div className="base-container">
+            <div className="header">Login</div>
+            <div className="content">
+                <div className="image">
+                    <img src={loginImg}/>
+                </div>
+                <div className="form">
+                    <div className="form-group">
+                        <lebel htmlFor="username">Username</lebel>
+                        <input type="text" name="username" placeholder="username"/>
+                    </div>
+
+                    <div className="form-group">
+                        <lebel htmlFor="password">Password</lebel>
+                        <input type="password" name="password" placeholder="password"/>
+                    </div>
+
+
+                </div>
+            </div>
+            <div className="foorter">
+                <button type="button" className="btn">Login</button>
+            </div>
+        </div>
+    }
+}
